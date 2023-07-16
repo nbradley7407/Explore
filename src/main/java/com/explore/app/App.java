0707de492170081a8701bc8a3552f1c02f26db6f
nbradley7407 <nbradley7407@gmail.com>
2023-07-16 14:04:11 -0600

    .gitignore, main loop

--- src/main/java/com/explore/app/App.java
+++ src/main/java/com/explore/app/App.java
@@ -2,7 +2,8 @@ package com.explore.app;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
+import org.json.simple.parser.ParseException;
+import java.util.Scanner;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -29,14 +30,19 @@ import java.nio.charset.StandardCharsets;
 
 public class App {
     public static void main(String[] args) {
+        // get token
         String clientId = "e2aeffcd41484cf2a18e8e30c3288019";
-        String clientSecret = "30adea1d5ff54ddc9c0e0133d707bb56";
+        String clientSecret = "e823196c5aa14db6a3cd6eb57efa8bad";
         String accessToken = getAccessToken(clientId, clientSecret);
-        System.out.println(accessToken);
-        System.out.println(getArtist(accessToken, "4Z8W4fKeB5YxbusRsdQVPb"));
 
-        // enter a loop with user interface
-        // 
+        // TO DO: check to see if there's a playlist called "Explore." If there is, save the ID#. If there isn't, make one and save the ID#
+        System.out.println(accessToken);
+        String explorePlaylist = get(accessToken, "me", "playlists");
+        System.out.println(explorePlaylist);
+        
+
+        // Go to interface
+        mainLoop();
     }
 
     public static String getAccessToken(String clientId, String clientSecret) {
@@ -85,11 +91,21 @@ public class App {
         return null;
     }
 
-    public static String getArtist(String accessToken, String artistId) {
-        return get(accessToken, "artists", artistId);
+    public static String getArtistName(String accessToken, String artistId) {
+        String response = get(accessToken, "artists", artistId);
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject jsonResponse = (JSONObject) parser.parse(response.toString());
+            String artistName = (String) jsonResponse.get("name");
+            return artistName;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static String get(String accessToken, String subject, String propertyId) {
+        System.out.println(accessToken);
         try {
             String apiUrl = "https://api.spotify.com/v1/" + subject + "/" + propertyId;
             URL url = new URL(apiUrl);
@@ -118,6 +134,34 @@ public class App {
         return null;
     }
 
+    public static void mainLoop() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("What would you like to do?");
+            System.out.println("1: Edit Explore playlist");
+            System.out.println("2: Find new music");
+            System.out.println("3: Exit");
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    // do something
+                    scanner.close();
+                    return;
+                case 2:
+                    // something
+                    scanner.close();
+                    return;
+                case 3:
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Please enter a number between 1 and 2.");
+                    continue;
+            }
+        }
+    }
 
 }
 
